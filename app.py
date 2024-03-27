68387c3a3d1cf519b82e54754b7c29e0dd5b2570
ericmoore0709 <159437312+ericmoore0709@users.noreply.github.com>
2024-03-27 15:42:42 -0700

    Added question page and handling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
@@ -9,3 +9,12 @@ responses = list[str]
 @app.get('/')
 def index():
     return render_template('index.html', title='Index', survey=satisfaction_survey)
+
+@app.get('/questions/<int:id>')
+def question(id: int):
+    question = satisfaction_survey.questions[id]
+    return render_template('question.html', title='Question '+ id, question=question)
+
+@app.post('/answer')
+def answer():
+    answer = request.form
