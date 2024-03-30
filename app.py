44cbae6b4c92b552f2eec4776629f7aec12c12cf
ericmoore0709 <159437312+ericmoore0709@users.noreply.github.com>
2024-03-30 03:05:38 +0000

    Added data validation handling

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from surveys import satisfaction_survey
+from uuid import uuid4
 
 app = Flask(__name__)
+
+# session key
+app.secret_key = str(uuid4())
 
 # initialize response list
 responses = []
@@ -15,13 +19,23 @@ def index():
 
 @app.get('/questions/<int:id>')
 def question(id: int):
-    question = satisfaction_survey.questions[id]
+    question = satisfaction_survey.questions[len(responses)]
+
+    if id != len(responses):
+        return redirect('/questions/' + str(len(responses)))
+
     return render_template('question.html', title='Question ' + str(id), question=question)
 
 
 @app.post('/answer')
 def answer():
-    answer = request.form['answer']
+    answer = request.form.get('answer')
+
+    if not answer:
+        session['errors'] = ["An answer is required."]
+        return redirect('/questions/' + str(len(responses)))
+
+    session.pop('errors', default=None)
     responses.append(answer)
     if len(responses) >= len(satisfaction_survey.questions):
         return redirect('/thanks')
